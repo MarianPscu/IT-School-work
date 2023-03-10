76e2deba1ab045eb2b8d4884ded741a8147e1e62
Marian Pascu <marian_pscu96@outlook.com>
2023-03-10 20:00:39 +0200

    item replace finals with x

---
+++ Exercitii/Strings/productx.py
@@ -0,0 +1,14 @@
+product_code_list = [
+    "mmf2",
+    "xdfr",
+    "ef3r",
+    "efc2",
+    "sddf"
+    "weee"
+]
+
+for word in product_code_list:
+    print(word.replace(word[-1], "x"))
+    
+
+
