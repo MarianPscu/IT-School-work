9ec7b03de6baacc756d3d99cb10abdae14576250
Marian Pascu <marian_pscu96@outlook.com>
2023-03-10 20:55:38 +0200

    capitalize letters

---
+++ Exercitii/Strings/capitalize.py
@@ -0,0 +1,15 @@
+product_code_list = [
+    "mmf2",
+    "xdfr",
+    "ef3r",
+    "efc2",
+    "sddf"
+    "weee"
+]
+
+for word in product_code_list:
+    print(word.capitalize())
+
+
+for word in product_code_list:
+    print(word.upper())
