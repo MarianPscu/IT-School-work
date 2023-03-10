44b4ecc867db6caa61bbea982428b3ee2ec4930d
Marian Pascu <marian_pscu96@outlook.com>
2023-03-10 20:59:54 +0200

    see if lorem has comma in first 20

---
+++ Exercitii/Strings/lorem_has_comma.py
@@ -0,0 +1,18 @@
+lorem = """Lorem Ipsum is simply dummy text of the printing and typesetting industry. 
+Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, 
+when an unknown printer took a galley of type and scrambled it to make a type specimen book. 
+It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. 
+It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, 
+and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum."""
+
+
+joined = "".join(lorem)
+
+twenty_chars = (joined[:20])
+
+
+if "," in twenty_chars:
+    print("True")
+
+else:
+    print("False")
