889bfa2be72448a6f2ad55d29eddcb4b512bffc0
Marian Pascu <marian_pscu96@outlook.com>
2023-03-11 07:15:51 +0200

    group product keys

---
+++ Exercitii/Strings/group_products.py
@@ -0,0 +1,15 @@
+product_code_list = [
+    "mmf2",
+    "xdfr",
+    "ef3r",
+    "efc2",
+    "sddf"
+    "weee"
+]
+
+empty_dict = {}
+
+for code in product_code_list:
+    empty_dict[code[-1]] = code
+
+print(empty_dict)
